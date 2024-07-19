f692ea5b05a7ab4166005758fa1184f7ef5dda2d
VidhiUndavia <97037845+VidhiUndavia@users.noreply.github.com>
2024-07-19 08:08:14 +0530

    code

---
+++ oop1.py
@@ -0,0 +1,11 @@
+class Animal:
+    def eat(self,name):
+        print(name," can eat.")
+    def sleep(self):
+        print("Animal can sleep")
+tiger=Animal()
+tiger.eat("Tiger")
+tiger.sleep()
+lion=Animal()
+lion.eat("lion")
+    
