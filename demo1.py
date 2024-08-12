b07521859381db077ba0856c62550c4f6e599ace
VidhiUndavia <97037845+VidhiUndavia@users.noreply.github.com>
2024-08-12 08:07:01 +0530

    Add files via upload

---
+++ demo1.py
@@ -0,0 +1,20 @@
+import mymath,fun2
+ 
+
+
+num1=int(input("Enter the first number"))
+num2=int(input("Enter the second number"))
+add=mymath.addition(num1,num2)
+print("Addition = ",add)
+
+
+#mymath.subtraction()
+
+# num1=int(input("Enter the first number"))
+# num2=int(input("Enter the second number"))
+# mymath.multiplication(num1,num2)
+
+# div=mymath.division() 
+# print("Division = ",div)
+
+fun2.getcube()
---
+++ mymath.py
@@ -0,0 +1,21 @@
+def addition(num1,num2):
+   
+    add=num1+num2
+    return add
+
+def subtraction():
+    num1=int(input("Enter the first number"))
+    num2=int(input("Enter the second number"))
+    sub=num1-num2
+    print("Subtraction = ",sub)
+
+def multiplication(num1,num2):
+   
+    mul=num1*num2
+    print("Multiplication = ",mul)
+
+def division():
+    num1=int(input("Enter the first number"))
+    num2=int(input("Enter the second number"))
+    div=num1/num2
+    return div
